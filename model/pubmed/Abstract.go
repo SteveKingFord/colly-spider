e70304bc5f382e058e6b6232845d542492196790
SteveKingFord <332312568@qq.com>
2022-09-10 17:34:58 +0800

    feat: type text

--- model/pubmed/Abstract.go
+++ model/pubmed/Abstract.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type PubmedAbstract struct {
 	gorm.Model
 	PubmedArticleId uint   `json:"pubmed_article_id"`
-	Content         string `json:"content"`
+	Content         string `json:"content" gorm:"type:text"`
 	Strong          string `json:"strong"`
 }
